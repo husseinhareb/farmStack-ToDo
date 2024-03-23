493fd699e3b5b1fea968fe73862d335c34edf6d8
husseinhareb <shtam.hareb12@gmail.com>
2024-03-23 22:01:51 +0200

    database

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware 
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from model import Todo
+from database import (
+    fetch_all_todos,
+    fetch_one_todo,
+    create_todo,
+    remove_todo,
+    update_todo
+)
 
-app = FastAPI() 
+app = FastAPI()
 
 origins = ['https://localhost:3000']
 
 app.add_middleware(
-    CORSMiddleware, 
+    CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
@@ -17,22 +25,35 @@ app.add_middleware(
 def read_root():
     return {"Ping": "Pong"}
 
-@app.get("/api/todo")
+@app.get("/api/todo", response_model=list[Todo])
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo/{title}", response_model=Todo)
+async def get_todo_by_id(title: str):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with the title {title}")
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong")
 
-@app.put("/api/todo{id}")
-async def put_todo(id,data):
-    return 1
+@app.put("/api/todo/{title}", response_model=Todo)
+async def put_todo(title: str, data: Todo):
+    response = await update_todo(title, data.dict())
+    if response:
+        return response
+    raise HTTPException(404, f"There is no TODO item with the title {title}")
 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+@app.delete("/api/todo/{title}")
+async def delete_todo(title: str):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted the item!"
+    raise HTTPException(404, f"There is no TODO item with the title {title}")
