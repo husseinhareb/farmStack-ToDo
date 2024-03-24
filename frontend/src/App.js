e77cee8b2ad58f5411351047cbb18b41a9f19aac
husseinhareb <shtam.hareb12@gmail.com>
2024-03-24 14:21:59 +0200

    add todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
+// @bekbrace
+// FARMSTACK Tutorial - Sunday 13.06.2021
+ 
+import React, { useState, useEffect} from 'react';
 import './App.css';
-import React,{useEffect,useState} from 'react';
+import TodoView from './components/TodoListView.js';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 
 function App() {
+
+  const [todoList, setTodoList] = useState([{}])
+  const [title, setTitle] = useState('') 
+  const [desc, setDesc] = useState('')
+  
+    
+
+  // Read all todos
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/todo')
+      .then(res => {
+        setTodoList(res.data)
+      })
+  });
+
+  // Post a todo
+  const addTodoHandler = () => {
+    axios.post('http://localhost:8000/api/todo/', { 'title': title, 'description': desc })
+      .then(res => console.log(res))
+};
 
   return (
     <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
