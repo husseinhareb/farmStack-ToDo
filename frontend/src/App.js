99072f3400109eb6246b17b2fc59234df54990f9
husseinhareb <shtam.hareb12@gmail.com>
2024-03-24 14:06:44 +0200

    app jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
+      <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
+      <h6 className="card text-white bg-primary mb-3">FASTAPI - React - MongoDB</h6>
+     <div className="card-body">
+      <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
+      <span className="card-text"> 
+        <input className="mb-2 form-control titleIn" onChange={event => setTitle(event.target.value)} placeholder='Title'/> 
+        <input className="mb-2 form-control desIn" onChange={event => setDesc(event.target.value)}   placeholder='Description'/>
+      <button className="btn btn-outline-primary mx-2 mb-3" style={{'borderRadius':'50px',"font-weight":"bold"}}  onClick={addTodoHandler}>Add Task</button>
+      </span>
+      <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
+      <div >
+      <TodoView todoList={todoList} />
+      </div>
+      </div>
+      <h6 className="card text-dark bg-warning py-1 mb-0" >Copyright 2021, All rights reserved &copy;</h6>
     </div>
   );
 }
