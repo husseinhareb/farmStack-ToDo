223b080d8ed53c0e8eee6aefdccb78a708172e31
husseinhareb <shtam.hareb12@gmail.com>
2024-03-24 14:07:00 +0200

    app jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  
+
   return (
     <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
       <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
@@ -21,7 +21,6 @@ function App() {
       <TodoView todoList={todoList} />
       </div>
       </div>
-      <h6 className="card text-dark bg-warning py-1 mb-0" >Copyright 2021, All rights reserved &copy;</h6>
     </div>
   );
 }
