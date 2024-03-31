035452f52cbfce5cabeae653a1ad0f61ab485b29
husseinhareb <shtam.hareb12@gmail.com>
2024-03-31 03:28:21 +0300

    fixing empty task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TodoView from './components/TodoListView';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
 
   const [todoList, setTodoList] = useState([{}])
-  const [title, setTitle] = useState('') 
+  const [title, setTitle] = useState('')
   const [desc, setDesc] = useState('')
-  
-    
+
+
 
   // Read all todos
   useEffect(() => {
@@ -19,38 +19,53 @@ function App() {
       .then(res => {
         setTodoList(res.data)
       })
-  },[]);
+  }, []);
 
   // Post a todo
   const addTodoHandler = () => {
+    if (!title.trim()) {
+      alert("Title cannot be empty or start with a space");
+      return;
+    }
+
+    if (!desc.trim()) {
+      alert("Description cannot be empty");
+      return;
+    }
+
     axios.post('http://localhost:8000/api/todo/', { 'title': title, 'description': desc })
       .then(res => console.log(res))
-};
-  const handleTaskTitle =(e) =>{
+      .catch(error => console.error(error));
+
+    setTitle('');
+    setDesc('');
+  };
+
+  const handleTaskTitle = (e) => {
     const trimmedValue = e.target.value.trimStart();
     setTitle(trimmedValue);
   }
-  const handleTaskDesc = (e) =>{
+  const handleTaskDesc = (e) => {
     const trimmedValue = e.target.value.trimStart();
     setDesc(trimmedValue);
   }
-return (
-  <div className="App" style={{ backgroundColor: "#4C5B61", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-    <div className="list-group-item" style={{ backgroundColor: "#DBFCFF" }}>
-      <div className="card-body">
-        <span className="card-text">
-          <input className="mb-2" onChange={handleTaskTitle} value={title} placeholder='Enter a title' />
-          <input className="mb-2" onChange={handleTaskDesc} value={desc} placeholder='Enter a description' />
-          <button className="btn" onClick={addTodoHandler}>Add Task</button>
-        </span>
-        <h5 className="card">Your Tasks</h5>
-        <div>
-          <TodoView todoList={todoList} />
+  return (
+    <div className="App" style={{ backgroundColor: "#4C5B61", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div className="list-group-item" style={{ backgroundColor: "#DBFCFF" }}>
+        <div className="card-body">
+          <span className="card-text">
+            <input className="mb-2" onChange={handleTaskTitle} value={title} placeholder='Enter a title' />
+            <input className="mb-2" onChange={handleTaskDesc} value={desc} placeholder='Enter a description' />
+            <button className="btn" onClick={addTodoHandler}>Add Task</button>
+          </span>
+          <h5 className="card">Your Tasks</h5>
+          <div>
+            <TodoView todoList={todoList} />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 
 
 }
